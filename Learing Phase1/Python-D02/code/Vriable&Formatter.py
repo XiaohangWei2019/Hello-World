8a59d18ee56aeb91775651d0eb988e01d14fb830
XiaohangWei2019 <50563986+XiaohangWei2019@users.noreply.github.com>
2019-05-24 21:27:50 +0800

    Create Vriable&Formatter.py

---
+++ Learing Phase1/Python-D02/code/Vriable&Formatter.py
@@ -0,0 +1,58 @@
+"""
+格式化输出
+Version: 0.1
+Copy from Author: 骆昊
+Date: 2018-02-27
+"""
+'''
+study only
+modified date:2019/5/24
+'''
+
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
+'''
+格式符
+格式符为真实值预留位置，并控制显示的格式。格式符可以包含有一个类型码，用以控制显示的类型，如下:
+
+%s    字符串 (采用str()的显示)
+
+%r    字符串 (采用repr()的显示)
+
+%c    单个字符
+
+%b    二进制整数
+
+%d    十进制整数
+
+%i    十进制整数
+
+%o    八进制整数
+
+%x    十六进制整数
+
+%e    指数 (基底写为e)
+
+%E    指数 (基底写为E)
+
+%f    浮点数
+
+%F    浮点数，与上相同
+
+%g    指数(e)或浮点数 (根据显示长度)
+
+%G    指数(E)或浮点数 (根据显示长度)
+
+%%    字符"%"
+--------------------- 
+作者：SuperCreators 
+来源：CSDN 
+study only
+'''
