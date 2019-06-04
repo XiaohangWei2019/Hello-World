f54241ac126fa00b730fbbb9a3b120dcf17ee2c1
XiaohangWei2019 <50563986+XiaohangWei2019@users.noreply.github.com>
2019-06-04 22:26:41 +0800

    Create is_Prime.py

---
+++ Learing Phase1/Python-D04/is_Prime.py
@@ -0,0 +1,11 @@
+num = int(input("please enter a number"))
+is_Prime = True
+for i in range(2, num):
+  if num % i == 0:
+    is_prime = False
+    break
+if is_Prime = Ture and num!=1:
+  print("is prime")
+else：
+  print("is not a prime")
+    
