739101c65a1273124e05cb162518811e941930a6
XiaohangWei2019 <50563986+XiaohangWei2019@users.noreply.github.com>
2019-06-05 22:06:35 +0800

    Delete Lilynum.py

--- Learing Phase1/Python-D05/Lilynum.py
+++
@@ -1,9 +0,0 @@
-num = int(input(""))
-high = num//100
-mid  = (num%100)//10
-low  = num%10
-new  = high**3 + mid**3 + low**3
-if new == num:
-  print('is lily num')
-else:
-  print('is not lily num')
